eb0ca1a49849da6e6e743067e31ef512cd09cf47
Samuel <devojukwu@gmail.com>
2023-11-28 04:06:01 +0100

    new update

--- frontend/src/component/pages/RewardPage.jsx
+++ frontend/src/component/pages/RewardPage.jsx
@@ -24,7 +24,7 @@ const RewardPage = () => {
         <input
           type="number"
           value={plasticRemittedNum}
-          onChange={(event) => handlePlasticRemit(parseInt(event.target.value))}
+          onChange={(e) => handlePlasticRemit(parseInt(e.target.value))}
         />
       </div>
 
